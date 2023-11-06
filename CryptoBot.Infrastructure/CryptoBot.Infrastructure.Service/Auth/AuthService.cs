be94e4cd80ba09cc0cb977e2f90490d99f3e708a
Luiz Moura <muniz.mourafelipe@gmail.com>
2023-11-06 15:10:18 -0300

    Added authorization methods and users validation

---
+++ CryptoBot.Infrastructure/CryptoBot.Infrastructure.Service/Auth/AuthService.cs
@@ -0,0 +1,60 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using CryptoBot.Domain;
+using System.Data;
+using Isopoh.Cryptography.Argon2;
+
+namespace CryptoBot.Infrastructure.Service.Auth;
+
+public class AuthService : IAuthService
+{
+    private readonly JwtConfig _jwtConfig;
+    private readonly IUserRepository _userRepository;
+
+    public AuthService(
+        JwtConfig jwtConfig,
+        IUserRepository userRepository)
+    {
+        _jwtConfig = jwtConfig;
+        _userRepository = userRepository;
+    }
+
+    private string GenerateToken(UserEntity user)
+    {
+        List<Claim> claims = new() { new(ClaimTypes.Name, user.UserName.ToString()) };
+        if (!string.IsNullOrEmpty(user.Role)) claims.Add(new(ClaimTypes.Role, user.Role.ToString()));
+
+        var key = new SymmetricSecurityKey(_jwtConfig.SecretBytes);
+        var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        var token = new JwtSecurityToken(claims: claims,
+                                         expires: DateTime.UtcNow.AddDays(1),
+                                         signingCredentials: cred);
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+        return jwt;
+        // if (!string.IsNullOrEmpty(user.Role)) claims.Add(new(ClaimTypes.Role, user.Role.ToString()));
+
+        // var tokenDescriptor = new SecurityTokenDescriptor
+        // {
+        //     Subject = new ClaimsIdentity(claims),
+        //     Expires = DateTime.UtcNow.AddHours(2),
+        //     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(_jwtConfig.SecretBytes),
+        //                                                 SecurityAlgorithms.HmacSha256Signature)
+        // };
+        // var token = tokenHandler.CreateToken(tokenDescriptor);
+        // return tokenHandler.WriteToken(token);
+    }
+
+    public string Authenticate(string userName, string password)
+    {
+        var user = _userRepository.Find(userName)
+                   ?? throw new NoNullAllowedException("Usuário inválidos");
+
+        if (!Argon2.Verify(user.PasswordHash, password))
+            throw new Exception("Senha inválida");
+
+        return GenerateToken(user);
+    }
+
+    public string HashPassword(string password) => Argon2.Hash(password);
+}
---
+++ CryptoBot.Infrastructure/CryptoBot.Infrastructure.Service/User/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using CryptoBot.Domain;
+using CryptoBot.Domain.Interfaces.Services.Base;
+
+namespace CryptoBot.Infrastructure.Service.User;
+
+public class UserService : Service<UserEntity, int>, IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IAuthService _authService;
+
+    public UserService(
+        IUserRepository userRepository,
+        IAuthService authService) : base(userRepository)
+    {
+        _userRepository = userRepository;
+        _authService = authService;
+    }
+
+    public void Register(string username, string password)
+    {
+        UserEntity user = new()
+        {
+            UserName = username,
+            PasswordHash = _authService.HashPassword(password),
+        };
+        _userRepository.Insert(user);
+    }
+}
