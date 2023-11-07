6a7e6380660e9635771cfa3fe30c60a876463c6a
Luiz Moura <muniz.mourafelipe@gmail.com>
2023-11-06 23:45:39 -0300

    Added HealthCheck controller

---
+++ CryptoBot.Host/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoBot.Host;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public ActionResult Check() => NoContent();
+}
